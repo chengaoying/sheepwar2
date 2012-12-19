cd50bafb83f3b8c1735daa6b33a5618383779010
af <404755455@qq.com>
2012-12-19 19:05:02 +0800

    update

--- src/main/java/sheepwar/StateShop.java
+++ src/main/java/sheepwar/StateShop.java
@@ -2,8 +2,8 @@ package sheepwar;
 
 import javax.microedition.lcdui.Image;
 
+import cn.ohyeah.stb.game.Recharge;
 import cn.ohyeah.stb.game.SGraphics;
-import cn.ohyeah.stb.game.StateRecharge;
 import cn.ohyeah.stb.key.KeyCode;
 import cn.ohyeah.stb.key.KeyState;
 import cn.ohyeah.stb.res.UIResource;
@@ -199,7 +199,8 @@ public class StateShop implements Common{
 				engine.isRecharge = false;
 				running = false;*/
 				clear();
-				StateRecharge sr = new StateRecharge(engine);
+				//StateRecharge sr = new StateRecharge(engine);
+				Recharge sr = new Recharge(engine);
 				sr.recharge();
 			}else if(shopX==2 && shopY==1){
 				running = false;
