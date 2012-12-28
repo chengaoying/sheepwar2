dc390e463b9621348a1803f8acd69dc068ea32e9
af <404755455@qq.com>
2012-12-28 16:41:38 +0800

    update

--- src/main/java/sheepwar/StateHelp.java
+++ src/main/java/sheepwar/StateHelp.java
@@ -46,16 +46,17 @@ public class StateHelp implements Common{
 	}
 
 	int x1 = 20, x2 = 550, x3 = 424;
+
 	private void showHelp(SGraphics g) {
 		Image help_bg = Resource.loadImage(Resource.id_achievement_bottom);
 		Image shop_big = Resource.loadImage(Resource.id_shop_big);       //{137,108}
+		Image help_cloth = Resource.loadImage(Resource.id_help_cloth);       //{137,108}
 		Image game_help = Resource.loadImage(Resource.id_game_help);     //{214,18}
 		Image achievement_out1 = Resource.loadImage(Resource.id_achievement_out1);   //{17,498}
 		Image achievement_left_right = Resource.loadImage(Resource.id_achievement_left_right);   //{}
-		Image achievement_left_right1 = Resource.loadImage(Resource.id_achievement_left_right1);   //{380,452}
+		Image achievement_left_right1 = Resource.loadImage(Resource.id_achievement_left_right1);   //
 		Image slash = Resource.loadImage(Resource.id_slash);
 		Image pass_cloud = Resource.loadImage(Resource.id_pass_cloud);
-		engine.setFont(20,true);					//设置字体大小
 		g.drawImage(help_bg, 0, 0, 20);
 
 		/*中间的云*/
@@ -80,8 +81,10 @@ public class StateHelp implements Common{
 		g.drawImage(pass_cloud, x3, 265, 20);
 		
 		g.drawImage(shop_big, 137, 108, 20);
+		g.drawImage(help_cloth, 157, 108+26, 20);
 		
-		int helpLeftRightX = 380,helpLeftRightY = 452,sapceLeftRight = 52;				//帮助界面中的按钮横纵坐标,sapceLeftRight左右间距
+		int helpLeftRightX = 380,helpLeftRightY = 452,sapceLeftRight = 52;				
+
 		int achLeftW = achievement_left_right.getWidth()/2;
 		engine.stateGame.drawNum(g, helpX+1, helpLeftRightX+achLeftW+1, helpLeftRightY+6);
 		g.drawImage(slash, helpLeftRightX+achLeftW+15, helpLeftRightY+4, 20);
@@ -94,15 +97,15 @@ public class StateHelp implements Common{
 					helpLeftRightY/*-helpShadowY*/, 20);
 		}else if(pageIndex == 1){
 			g.drawRegion(achievement_left_right, 0, 0, achLeftW, 		//翻页左按钮
-					achievement_left_right.getHeight(), 0, helpLeftRightX/*-helpShadowX*/, helpLeftRightY/*-helpShadowY*/, 20);
+					achievement_left_right.getHeight(), 0, helpLeftRightX/*-helpShadowX*/,helpLeftRightY/*-helpShadowY*/, 20);
 			g.drawRegion(achievement_left_right1, 1*achLeftW, 0, achLeftW,
-					achievement_left_right.getHeight(), 0, helpLeftRightX+sapceLeftRight+achLeftW,
-					helpLeftRightY, 20);
+					achievement_left_right.getHeight(), 0, helpLeftRightX+sapceLeftRight+achLeftW,helpLeftRightY, 20);
 		}
-		g.drawImage(game_help, 235,18, 20);
+		g.drawImage(game_help, 220,18, 20);
 		g.drawImage(achievement_out1, 17,498, 20);
 		g.setColor(0x000000);
-		TextView.showMultiLineText(g, Resource.gameInfo[helpX], 6,160, 135, 359, 333);	
+		engine.setFont(10,true);
+		TextView.showMultiLineText(g, Resource.gameInfo[helpX], 5,177, 150, 315, 248);
 		if(Configurations.getInstance().isTelcomOperatorsTelcomgd()){
 			engine.setFont(20,true);
 			g.drawString("游戏版本 1.0.0", 177, 388, 20);
--- src/main/java/sheepwar/StateShop.java
+++ src/main/java/sheepwar/StateShop.java
@@ -53,16 +53,17 @@ public class StateShop implements Common{
 		Image game_bg = Resource.loadImage(Resource.id_shop_bottom);
 		Image shop_balance = Resource.loadImage(Resource.id_shop_balance);//{46,454}
 		Image shop_big = Resource.loadImage(Resource.id_shop_big);//{29,103}
-		Image shop_go_pay = Resource.loadImage(Resource.id_shop_go_pay);//{457,381}				//9/29被修改为单纯的文字图片
-		Image shop_go_pay_base = Resource.loadImage(Resource.id_achievement_left);				//单纯的 按钮图片
-		Image shop_go_pay_base1 = Resource.loadImage(Resource.id_achievement_left1);				//单纯的 按钮图片
-		
+		Image shop_go_pay = Resource.loadImage(Resource.id_shop_go_pay);//{457,381}				
+		Image shop_go_pay_base = Resource.loadImage(Resource.id_achievement_left);				
+		Image shop_go_pay_base1 = Resource.loadImage(Resource.id_achievement_left1);				
 		Image shop_midding = Resource.loadImage(Resource.id_shop_midding);//{434,103}
-		Image shop_out = Resource.loadImage(Resource.id_shop_out);//{457,429}					//9/29被修改为单纯的文字图片“返回”
+		Image shop_cloth = Resource.loadImage(Resource.id_shop_cloth);//{434,103}
+		Image shop_out = Resource.loadImage(Resource.id_shop_out);//{457,429}					
 		Image shop_small = Resource.loadImage(Resource.id_shop_small);
 		Image price_quantity = Resource.loadImage(Resource.id_price_quantity);
 		Image shop = Resource.loadImage(Resource.id_shop);//{217,18}
 		Image playing_prop=Resource.loadImage(Resource.id_playing_prop);
+		Image shop_stroke=Resource.loadImage(Resource.id_stroke);					
 		Image shop_selected=Resource.loadImage(Resource.id_shop_selected);
 		g.setColor(0xffffff);
 		g.drawImage(game_bg, 0, 0, 20);
@@ -71,65 +72,69 @@ public class StateShop implements Common{
 		g.drawImage(shop_big, 29-12, 103, 20);
 		g.drawImage(shop_balance, 46, 457, 20);
 		g.drawImage(shop_midding, 434-12, 103, 20);
+		g.drawImage(shop_cloth, 434+10, 103+26, 20);
 		
-		int x =42, y = 130, spaceX = 15, spaceY = 5;
+		int x =52, y = 130, spaceX = 15, spaceY = 5;
 		int smallW = shop_small.getWidth(), smallH = shop_small.getHeight();
 		int p_propW = playing_prop.getWidth()/8, p_propH = playing_prop.getHeight();
-		int priAndQuW = price_quantity.getWidth()/2,priAndQuH = price_quantity.getHeight();
+		int  strokeW = shop_stroke.getWidth(),strokeH = shop_stroke.getHeight();
+		int priAndQuW = price_quantity.getWidth(),priAndQuH = price_quantity.getHeight();
 		for(int i=0;i<4;i++){
 		     for(int j=0;j<2;j++){
-				if(shopX==j && shopY==i){
-					engine.setFont(30,true);	
-					g.setColor(0xffffff);
-					g.drawImage(shop_selected, x+(spaceX+smallW)*j, y+(spaceY+smallH)*i, 20);
-					g.drawRegion(price_quantity, 0, 0, priAndQuW, priAndQuH, 0,  x+(spaceX+smallW)*j+80, y+(spaceY+smallH)*i+12, 20);
-					g.drawRegion(playing_prop, getPropIndex(i, j)*p_propW, 0, p_propW, p_propH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);
-					g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getPrice()),
-							x+(spaceX+smallW)*j+134, y+(spaceY+smallH)*i+14, 20);
-					g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getNums()), 
-							x+(spaceX+smallW)*j+134, y+(spaceY+smallH)*i+40, 20);
-					g.setColor(0x000000);
-					engine.setFont(20, true);
-					TextView.showMultiLineText(g, Resource.propIntroduce[shopY][shopX]+engine.getEngineService().getExpendAmountUnit(), 5, 444, 130, 162, 220);
-					engine.setDefaultFont();
-				}else{
-					engine.setFont(30,true);
-					int col = g.getColor();
-					g.setColor(0x000000);
-					g.drawImage(shop_small, x+(spaceX+smallW)*j, y+(spaceY+smallH)*i, 20);
-					g.drawRegion(price_quantity, priAndQuW, 0, priAndQuW, priAndQuH, 0, x+(spaceX+smallW)*j+80, y+(spaceY+smallH)*i+12,20);
-					g.drawRegion(playing_prop, getPropIndex(i, j)*p_propW, 0, p_propW, p_propH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);
-					g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getPrice()),
-							x+(spaceX+smallW)*j+134, y+(spaceY+smallH)*i+14, 20);
-					g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getNums()), 
-							x+(spaceX+smallW)*j+134, y+(spaceY+smallH)*i+40, 20);
-					engine.setDefaultFont();
-					g.setColor(col);
-				}
+		    	 if(shopX==j && shopY==i){
+						engine.setFont(30,true);	
+						g.setColor(0xffffff);
+						g.drawImage(shop_selected, x+(spaceX+smallW)*j, y+(spaceY+smallH)*i, 20);
+						g.drawRegion(price_quantity, 0, 0, priAndQuW, priAndQuH, 0,  x+(spaceX+smallW)*j+80, y+(spaceY+smallH)*i+12, 20);
+						g.drawRegion(playing_prop, getPropIndex(i, j)*p_propW, 0, p_propW, p_propH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);//
+						g.drawRegion(shop_stroke, /*getPropIndex(i, j)*p_propW*/0, 0,strokeW, strokeH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);
+						g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getPrice()),
+								x+(spaceX+smallW)*j+136, y+(spaceY+smallH)*i+16, 20);
+						g.drawString(String.valueOf(engine.props[getPropIndex(i, j)].getNums()), 
+								x+(spaceX+smallW)*j+136, y+(spaceY+smallH)*i+42, 20);
+						g.setColor(0x000000);
+						engine.setFont(25, true);
+						TextView.showMultiLineText(g, Resource.propIntroduce[shopY][shopX]+engine.getEngineService().getExpendAmountUnit(), 2, 452, 142, 140, 195);
+						engine.setDefaultFont();
+					}else{
+						engine.setFont(30,true);
+						int col = g.getColor();
+						g.setColor(0x000000);
+						g.drawImage(shop_small, x+(spaceX+smallW)*j, y+(spaceY+smallH)*i, 20);
+						g.drawRegion(price_quantity,0/* priAndQuW*/, 0, priAndQuW,priAndQuH, 0, x+(spaceX+smallW)*j+80, y+(spaceY+smallH)*i+12,20);
+						g.drawRegion(playing_prop, getPropIndex(i, j)*p_propW, 0,p_propW, p_propH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);
+						g.drawString(String.valueOf(engine.props[getPropIndex(i,j)].getPrice()),
+								x+(spaceX+smallW)*j+136, y+(spaceY+smallH)*i+16,20);
+						g.drawString(String.valueOf(engine.props[getPropIndex(i,j)].getNums()), 
+								x+(spaceX+smallW)*j+136, y+(spaceY+smallH)*i+42,20);
+						engine.setDefaultFont();
+						g.setColor(col);
+					}
+		    	 g.drawRegion(shop_stroke, /*getPropIndex(i, j)*p_propW*/0, 0, strokeW, strokeH, 0,x+(spaceX+smallW)*j+16, y+(spaceY+smallH)*i+6, 20);
 			}
 		}
-		if(shopX==2){          //充值和返回被选择的阴影效果
+		if(shopX==2){          // changed on 11-7 by Lee : removed the slash effect
 			 if(shopY==0){      //控制方向由左到右的入口方向
 				 g.drawImage(shop_go_pay_base1, 457-8, 381-5, 20);
 			   	 g.drawImage(shop_go_pay, 457-8+16, 381-5+5, 20);
 			 	 g.drawImage(shop_go_pay_base, 457-8, 429-5, 20);
 			   	 g.drawImage(shop_out, 457+16, 429+7, 20);
 			  }else{
-				 g.drawImage(shop_go_pay_base, 457-8, 381-5, 20);
-				 g.drawImage(shop_go_pay, 457-8+16, 381-5+5, 20);
-				 g.drawImage(shop_go_pay_base1, 457-8, 429-5, 20);
-				 g.drawImage(shop_out, 457+16, 429+7, 20);
+			   	 g.drawImage(shop_go_pay_base, 457-8, 381-5, 20);
+			   	 g.drawImage(shop_go_pay, 457-8+16, 381-5+5, 20);
+			 	 g.drawImage(shop_go_pay_base1, 457-8, 429-5, 20);
+			   	 g.drawImage(shop_out, 457+16, 429+7, 20);
 			 }
 		}else{
-	    	g.drawImage(shop_go_pay_base, 457 -8, 381-5, 20);
-	    	g.drawImage(shop_go_pay, 457-8+16, 381-5+5, 20);
-	   		g.drawImage(shop_go_pay_base, 457-8, 429-5, 20);
-	   		g.drawImage(shop_out, 457-8+16, 429-5+7, 20);
+	   	 g.drawImage(shop_go_pay_base, 457-8, 381-5, 20);
+	   	 g.drawImage(shop_go_pay, 457-8+16, 381-5+5, 20);
+	   	 g.drawImage(shop_go_pay_base, 457-8, 429-5, 20);
+	   	 g.drawImage(shop_out, 457+16, 429+7, 20);
 		}
 		 
 		engine.setFont(30, true);
 		g.setColor(0x000000);
-		g.drawString(String.valueOf(engine.getEngineService().getBalance())+engine.getEngineService().getExpendAmountUnit(),110,449+7, 20);//用户余额
+		g.drawString(String.valueOf(engine.getEngineService().getBalance())+engine.getEngineService().getExpendAmountUnit(),110,459, 20);//用户余额
 		engine.setDefaultFont();
 	}
 	
